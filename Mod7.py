05cddf3a6b7b2a08ccce794e54dc322ee80e7e35
geslyn <61592906+geslyn@users.noreply.github.com>
2020-03-02 00:27:20 -0500

    Update Mod7.py

--- Mod7.py
+++ Mod7.py
@@ -1,5 +1,4 @@
 import datetime
-e = datetime.datetime.now()
-print ("Current date and time = %s" % e)
-print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
-print ("The time is now: = %s:%s:%s" % (e.hour, e.minute, e.second))
+from datetime import datetime, timedelta
+data = datetime.now()
+print(data)
